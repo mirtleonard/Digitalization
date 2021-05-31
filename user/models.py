b5f4b80dfc3896f4b948600369e83e364ddb2d8f
mirtleonard <leonardmirt26@gmail.com>
2021-05-31 15:30:44 +0300

    add user model

---
+++ user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    branch = models.CharField(max_length=40)
+    reports = models.IntegerField(default=0)
+    birth = models.DateField(auto_now=True)
+    def __str__(self):
+        return self.username
