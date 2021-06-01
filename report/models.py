d8ce71b7a62b94483f53ecab1aa4813a4b75482c
mirtleonard <leonardmirt26@gmail.com>
2021-06-01 14:07:41 +0300

    add report model

--- report/models.py
+++ report/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Report(models.Model):
+    username = models.CharField(max_length=40);
+    areas = models.CharField(max_length=40);
+    location = models.CharField(max_length=40);
+    title = models.CharField(max_length=40);
+    duration = models.IntegerField();
+    date = models.DateField();
+    participants = models.CharField(max_length=40);
+    description = models.CharField(max_length=40);
+    goals = models.CharField(max_length=40);
+    strengths = models.CharField(max_length=40);
+    weaknesses = models.CharField(max_length=40);
+    improvements = models.CharField(max_length=40);
+    branch = models.CharField(max_length=40)
+    def __str__(self):
+        return self.title
