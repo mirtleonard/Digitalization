fccd72e26874f724302c0dfdd224fc6442336ba9
mirtleonard <leonardmirt26@gmail.com>
2021-06-15 17:48:34 +0300

    removed report

--- report/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from report.models import ActivityReport, EventReport
-
-# Register your models here.
-
-admin.site.register(ActivityReport)
-admin.site.register(EventReport)
