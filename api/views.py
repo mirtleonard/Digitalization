d47986b70adf74f657c74ba489523d70ddd6f9c6
mirtleonard <leonardmirt26@gmail.com>
2021-07-05 11:52:36 +0300

    added users api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
+from rest_framework import serializers, viewsets
 from django.shortcuts import render
+from user.models import User
 
-# Create your views here.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
