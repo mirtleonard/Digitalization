9906fc22e8d8c77f0c58444bcdac42a18b06e2fd
mirtleonard <leonardmirt26@gmail.com>
2021-08-27 13:31:25 +0300

    debug true

--- digitalization/settings.py
+++ digitalization/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u5^g_-1tgspl0wba$*7_*$&r!kzwuk4au(g@)&grmawf(k_ecb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['planeta-pildesti.herokuapp.com', '127.0.0.1', '192.168.1.9', ]
 
